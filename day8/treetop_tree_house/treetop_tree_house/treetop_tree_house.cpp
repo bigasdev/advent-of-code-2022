5f378a883e64866aa5b03df88710afa4a305a8ab
gustavo domingues <deathdiec@gmail.com>
2022-12-09 13:20:40 -0300

    need to learn why its not working now

--- day8/treetop_tree_house/treetop_tree_house/treetop_tree_house.cpp
+++ day8/treetop_tree_house/treetop_tree_house/treetop_tree_house.cpp
@@ -3,10 +3,11 @@
 
 #include <iostream>
 #include <vector>
+#include <array>
 #include "../../../day4/supply_stacks/supply_stacks/load_input.hpp"
 
 //for this one we'll need to store the strings to map it later
-void generateMap(string map[]);
+void generateMap(string map[], int count);
 
 //our tree class
 struct Tree {
@@ -20,6 +21,10 @@ public:
 
 //list of trees
 vector<Tree> trees;
+int count = 0;
+
+std::string line;
+std::array<std::array<int, 99>, 99> grid_2d;
 
 int main()
 {
@@ -30,13 +35,64 @@ int main()
     string map[100];
     int count = 0;
 
+    /*for (int i = 0; i < 99; i++)
+    {
+        std::getline(file, line);
+        for (int j = 0; j < 99; j++)
+        {
+            grid_2d[i][j] = (line[j] - '0');
+        }
+    }
+
+    int score = 197;
+    int tree_height = 0;
+
+    for (int i = 1; i <= 98; ++i)
+    {
+        for (int j = 1; j <= 98; ++j)
+        {
+            tree_height = grid_2d[i][j];
+
+            for (int x = i - 1; x >= 0; --x)
+            {
+                if (tree_height <= grid_2d[x][j]) { goto check2; }
+            }
+            score++; continue;
+
+        check2:;
+            for (int x = i + 1; x < 99; ++x)
+            {
+                if (tree_height <= grid_2d[x][j]) { goto check3; }
+            }
+            score++; continue;
+
+        check3:;
+            for (int y = j - 1; y >= 0; --y)
+            {
+                if (tree_height <= grid_2d[i][y]) { goto check4; }
+            }
+            score++; continue;
+
+        check4:;
+            for (int y = j + 1; y < 99; ++y)
+            {
+                if (tree_height <= grid_2d[i][y]) { goto turbo_break; }
+            }
+            score++; continue;
+
+        turbo_break:;
+        }
+    }*/
+
+    //cout << score << endl;
+
     while (getline(file, line)) {
         //we count the lines and add it to the array of strings
         map[count] = line;
         count++;
     }
     //generating the map
-    generateMap(map);
+    generateMap(map, count);
     //to not exit the program
     while (true) {
 
@@ -44,11 +100,11 @@ int main()
     std::cout << "Hello World!\n";
 }
 
-void generateMap(string map[])
+void generateMap(string map[], int count)
 {
-    int treeAmount = 0;
+    int treeAmount = 16;
 
-    for (size_t i = 0; i < map->length(); i++)
+    for (size_t i = 0; i < count; i++)
     {
         //loop to check the trees from outside and whatnot
         cout << map[i] << endl;
@@ -57,18 +113,18 @@ void generateMap(string map[])
                 cout << " Top of the grid ! " << endl;
                 for (size_t i = 0; i < map[i].length(); i++)
                 {
-                    treeAmount++;
+                    //treeAmount++;
                 }
-            }else if (i == map->length()-1) {
+            }else if (i == count-1) {
                 cout << " Bottom of the grid ! " << endl;
                 for (size_t i = 0; i < map[i].length(); i++)
                 {
-                    treeAmount++;
+                    //treeAmount++;
                 }
             }
             //normal loop, this is where we'll check the normal trees
             else {
-                treeAmount += 2;
+                //reeAmount += 2;
                 for (size_t j = 0; j < map[i].length(); j++)
                 {
                     //creating the tree struct
@@ -94,9 +150,10 @@ void generateMap(string map[])
                         if (up >= 0) {
                             tree.up = (int)map[up][j] - 48;
                         }
-                        if (down <= map->length()) {
+                        if (down <= count-1) {
                             tree.down = (int)map[down][j] - 48;
                         }
+                        cout << " just to see " << endl;
                         trees.push_back(tree);
                     }
                 }
@@ -110,10 +167,12 @@ void generateMap(string map[])
         cout << " " << t.down << " " << endl;
 
         if (t.middle > t.up && (t.middle > t.right || t.middle > t.left)) {
+            cout << " its visible on up ! " << endl;
             treeAmount++;
             continue;
         }
         if (t.middle > t.down && (t.middle > t.right || t.middle > t.left)) {
+            cout << " its visible on down ! " << endl;
             treeAmount++;
             continue;
         }
